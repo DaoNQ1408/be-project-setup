35bd6a02984782f149de04ee4b496505489f7244
DaoNQ1408 <nguyenquangdao1408@gmail.com>
2026-03-26 15:29:59 +0700

    add new version

---
+++ spring-boot-setup-v2/src/main/java/com/daonq/springbootsetupv2/SpringBootSetupV2Application.java
@@ -0,0 +1,13 @@
+package com.daonq.springbootsetupv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSetupV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSetupV2Application.class, args);
+    }
+
+}
